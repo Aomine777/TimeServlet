6366fc8722fa79e5778f0886e43f2fd2314f03be
Aomine7777 <141518740+Aomine7777@users.noreply.github.com>
2024-03-28 15:34:49 +0200

    add thymeleaf

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+E
+src/webapp/index.html,0\c\0cc4925ffd876dc10110f64fc429bb483fc38f18
+J
+src/webapp/WEB-INF/web.xml,9\7\97fa2086470652346d2d283d947bcf893a374a7e
+Z
+*src/main/java/org/example/TimeServlet.java,e\d\edee8be5b7e7d68ae4d9e8b6660a429ef4e580c3
+e
+5src/main/java/org/example/TimezoneValidateFilter.java,e\e\ee6589a2328034df4339d5cc195412636053b7ae
---
+++ src/main/java/org/example/TimeServlet.java
@@ -0,0 +1,72 @@
+package org.example;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;
+import org.thymeleaf.web.servlet.JavaxServletWebApplication;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+@WebServlet({"/time"})
+public class TimeServlet extends HttpServlet {
+
+    private final TemplateEngine templateEngine = new TemplateEngine();
+    private final Map<String, String> coockiesMap = new HashMap<>();
+
+
+    @Override
+    public void init() {
+        JavaxServletWebApplication javaxServletWebApplication =
+                JavaxServletWebApplication.buildApplication(this.getServletContext());
+        WebApplicationTemplateResolver resolver = new WebApplicationTemplateResolver(javaxServletWebApplication);
+
+        resolver.setPrefix("/WEB-INF/temp/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(templateEngine.getTemplateResolvers().size());
+        resolver.setCacheable(false);
+
+        templateEngine.addTemplateResolver(resolver);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
+        resp.setContentType("text/html");
+        String formattedDateTime;
+
+        String urlParam = req.getParameter("time");
+
+
+        if (req.getCookies() != null)
+            Arrays.stream(req.getCookies()).forEach((c) ->
+                    coockiesMap.put(c.getName(), c.getValue()));
+
+        if (urlParam == null) {
+            urlParam = coockiesMap.getOrDefault("lastTimezone", "UTC+0");
+            formattedDateTime = getFormattedTime(urlParam);
+        } else {
+            urlParam = urlParam.replace(" ", "+");
+            formattedDateTime = getFormattedTime(urlParam);
+            Cookie cookie = new Cookie("lastTimezone", urlParam);
+            cookie.setMaxAge(10 * 60);
+            resp.addCookie(cookie);
+        }
+
+        Context context = new Context(req.getLocale(), Map.of("timeText", formattedDateTime));
+        templateEngine.process("time", context, resp.getWriter());
+    }
+
+    private String getFormattedTime(String urlParam) {
+        LocalDateTime localNow = LocalDateTime.now(TimeZone.getTimeZone(ZoneId.of(urlParam)).toZoneId());
+        return localNow.format(DateTimeFormatter.ofPattern("YYYY-MM-DD HH:mm:ss")) + " " + urlParam;
+    }
+}
